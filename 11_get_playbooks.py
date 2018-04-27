e247535ec26b3f36df10cf8c68db6daf1a508c26
Unknown <michael@threatgrid.com>
2018-04-26 21:04:55 -0400

    Remove extra paramter

--- 11_get_playbooks.py
+++ 11_get_playbooks.py
@@ -2,7 +2,7 @@ import requests
 
 api_key = 'a1b2c3d4e5f6g7h8i9j0k1l2m3'
 
-url = 'https://panacea.threatgrid.com/api/v3/configuration/playbooks?api_key={}'.format(api_key,q)
+url = 'https://panacea.threatgrid.com/api/v3/configuration/playbooks?api_key={}'.format(api_key)
 
 r = requests.get(url)
 
