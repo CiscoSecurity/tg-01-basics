5c141070008cfcfe12df25c949bbfc295dc85ee6
Michael Auger <miauger@cisco.com>
2020-01-20 21:31:47 -0500

    Update method for submitting URL
    No longer have to create an InternetShortcut "file" to submit URLs can simply use the  "url" parameter

--- 07_submit_url.py
+++ 07_submit_url.py
@@ -6,12 +6,9 @@ url = 'https://panacea.threatgrid.com/api/v2/samples'
 
 sample_url = 'https://www.cisco.com'
 
-sample_template = '[InternetShortcut]\nURL={}'.format(sample_url)
+parameters = {'api_key': api_key,
+			  'url': sample_url}
 
-sample = {'sample': sample_template}
-
-parameters = {'api_key': api_key}
-
-r = requests.post(url, files=sample, params=parameters)
+r = requests.post(url, params=parameters)
 
 print(r.json())
