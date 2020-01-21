19873ef428bc74eb697cf7d1ce0c857b19e58d57
Michael Auger <miauger@cisco.com>
2020-01-20 21:33:11 -0500

    Fix formatting

--- 07_submit_url.py
+++ 07_submit_url.py
@@ -7,7 +7,7 @@ url = 'https://panacea.threatgrid.com/api/v2/samples'
 sample_url = 'https://www.cisco.com'
 
 parameters = {'api_key': api_key,
-			  'url': sample_url}
+              'url': sample_url}
 
 r = requests.post(url, params=parameters)
 
